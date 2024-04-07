224fea52b845cee835567a86ef121b3cd9d105e8
fnsl1026 <fnsl1026@naver.com>
2024-04-08 00:12:39 +0900

    Test : One Winner

--- src/test/java/step2/carRaceTest.java
+++ src/test/java/step2/carRaceTest.java
@@ -1,8 +1,17 @@
 package step2;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import step2.controller.RacingGame;
+import step2.domain.Car;
+import step2.domain.Winners;
 import step2.domain.randomNumGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,5 +23,26 @@ public class carRaceTest {
 
         assertThat(randomNum).isBetween(0,9);
     }
+    @ParameterizedTest
+    @DisplayName("한명의 우승:정해진 대본으로 test시 brown의 우승")
+    @CsvSource({"'neo,brie,brown',5"})
+    public void raceCarTest1(String name,int tryNum){
+        RacingGame racingGame= new RacingGame(name,tryNum);
+
+        int[][] script={{1,1,9},{1,1,9},{1,1,9},{1,1,9},{1,1,9}};
+        int cnt=0;
+        while(racingGame.racing()){
+            racingGame.testRace(script[cnt++]);
+        }
+        List<Car> winners = Winners.findWinners(racingGame.getCars());
+
+        List<String> winnersName = new ArrayList<>();
+        for(Car winner: winners){
+            winnersName.add(winner.getName());
+        }
+
+        Assertions.assertThat(winnersName).contains("brown");
+    }
+
 
 }
